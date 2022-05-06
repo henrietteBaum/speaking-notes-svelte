3d96181a56a0ec2eadc91fcab03a95f4e1d047de
henrietteBaum <henriette-baum@posteo.de>
2022-05-06 14:31:21 +0200

    add menu and preview

---
+++ src/components/Editor.svelte
@@ -0,0 +1,67 @@
+<script>
+  import { marked } from "marked";
+  import { NavItem, NavLink} from "sveltestrap"
+  import EditorMenu from "./EditorMenu.svelte"
+  let mdText = "";
+  let preview = false;
+
+  function handleTogglePreview() {
+    preview = !preview;
+  }
+</script>
+
+<div class="div-menu">
+{#if preview}
+  <NavItem>
+    <NavLink on:click={handleTogglePreview} href="#">Editor</NavLink>  
+  </NavItem>
+  {:else}
+  <NavItem>
+    <NavLink on:click={handleTogglePreview} href="#">Preview</NavLink>
+  </NavItem>
+  {/if}
+<EditorMenu />
+</div>
+
+<section>
+  {#if preview}
+  Preview
+  <div class="prev">
+    {@html marked(mdText)} 
+  </div>
+  {:else}
+  <textarea class="md-input" bind:value={mdText} />
+  {/if}
+</section>
+
+
+
+<style>
+  .div-menu {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+  }
+
+  button {
+    width: 5em;
+    height: 2em;
+    padding: auto;    
+    background-color: rgb(65, 5, 5);
+    color: orange;
+    border: none;
+  }
+  .md-input {
+    width: 100%;
+    height: 100vh;
+    background-color: rgb(50, 51, 59);
+    color: white;
+    border: none;
+  }
+  .prev {
+    width: 100%;
+    height: 100vh;
+    background-color: rgb(86, 1, 1);
+    color: white;
+  }
+</style>
---
+++ src/components/EditorMenu.svelte
@@ -0,0 +1,62 @@
+<script>
+  import {
+    Nav,
+    NavItem,
+    Dropdown,
+    DropdownItem,
+    DropdownToggle,
+    DropdownMenu,
+    NavLink
+  } from 'sveltestrap';
+
+
+</script>
+<div class="nav-bar">
+  <Nav dark tabs>
+    <NavItem class="nav-item">
+       
+    </NavItem>
+    <Dropdown dark nav inNavbar>
+      <DropdownToggle nav caret>File</DropdownToggle>
+      <DropdownMenu dark>
+        <DropdownItem>Open File</DropdownItem>
+        <DropdownItem>Save Text</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Clear Editor</DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+    <Dropdown dark nav inNavbar>
+      <DropdownToggle nav caret>Speak</DropdownToggle>
+      <DropdownMenu dark>
+        <DropdownItem>Speak Text</DropdownItem>
+        <DropdownItem>Pause</DropdownItem>
+        <DropdownItem>Stop</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Clear Editor</DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+    <NavItem>
+      <NavLink href="#">Help</NavLink>
+    </NavItem>
+    <NavItem>
+      <NavLink href="#">About</NavLink>
+    </NavItem>
+  </Nav>
+</div>
+
+
+<style>
+  .nav-bar {
+    background-color: rgb(65, 5, 5);
+    font-size: large;
+  }
+
+  :global(.nav-link) {
+    color: orange !important;
+  }
+
+  :global(.dropdown-item) {
+    color: orange !important;
+  }
+ 
+</style>
